393adb2f5fa0c09e43f6779e7abfd61567e9f053
Taylor Savage <taylorsavagedesign@gmail.com>
2019-01-25 12:42:58 -0600

    Initial file creation

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create 20 questions, 4 choices for each in HTML
+	// Place incorrect answers in one class and correct in another
+
+// Initialize score
+
+// Attach event listeners to all incorrect, and correct answer items
+	// If incorrect is clicked, disallow selection of other items within parent div, apply X and color
+	// If correct is clicked, disallow selection of other items, apply check and color, increment score
+
+// Once Final question has received click, present score
+	//Refer to buzzfeed quizzes for processes
